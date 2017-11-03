582d4517ed9029561385b6c0bb7ed50364c1bf8b
Allen Kim <allen.kim@rci.rogers.ca>
2017-11-03 16:05:49 -0400

    updated with examples

---
+++ examples/ce-polyfill.js
@@ -0,0 +1,60 @@
+/**
+ * Custom Element Polyfill
+ * It's Allen's customElemnts polyfill since Allen does not use any shadowDOM at the moment
+ */
+(function(){
+  let __customElements = {};
+
+  let applyCustomElement = function(el, klass) {
+    if (el.tagName.match(/-/)) {
+      // el.__proto__ = klass.prototype;
+      Object.setPrototypeOf(el, klass.prototype);
+      el._init();
+      setTimeout(function(){
+        el.connectedCallback && el.connectedCallback();
+      })
+    }
+  };
+
+  let CustomElements = { // polyfill of window.customElements. I only need .define
+    define: function(name, klass) {
+      __customElements[name] = klass;
+      //this is called after window.onload
+      Array.from(document.querySelectorAll(name)).forEach(function(el) {
+        applyCustomElement(el, __customElements[name]);
+      });
+    }
+  }
+
+  let observer = new MutationObserver(function(mutationRecords) {
+    mutationRecords.forEach(function(mutationRecord) {
+      if (mutationRecord.type == 'childList') { // e.g. attribures, characterData
+        Array.from(mutationRecord.removedNodes).forEach(function(node) {
+          let nodeName = node.nodeName.toLowerCase();
+          let klass = __customElements[nodeName];
+          if (klass) { // Ha, this is a customElement
+            applyCustomElement(node, klass);
+          }
+        });
+        Array.from(mutationRecord.addedNodes).forEach(function(node) {
+          node.disconnectedCallback && node.disconnectedCallback();
+        });
+      }
+    })
+  });
+
+  if (!window.customElements) {
+    window.customElements = CustomElements;
+    window.addEventListener('load', function() {
+      observer.observe(document.body, {childList: true});
+    });
+  }
+
+  if (!Object.values) {
+    Object.values = function(obj) {
+      return Object.keys(obj).map(function(key) {
+        return obj[key];
+      });
+    }
+  };
+})();
