d3d0a5486e6797c2384def254fb3591c8eb39b9a
Allen Kim <allen.kim@rci.rogers.ca>
2017-11-03 17:26:52 -0400

    updated with the secondary value for css properties

--- build.js
+++ build.js
@@ -12,18 +12,36 @@
 // minify core-components.html, and write to dist/core-components.min.html
 // combine webcomponents-hi.js anc ce-polyfill.js, write to dist/custom-elements-polyfill.js
 
+/**  insert polyfill if not defined  **/
 /*
+<script>
+if (!('import' in document.createElement('link')) {
+  let scriptEl = document.createElement('script');
+  scriptEl.setAttribute('src', '//unpkg.com/custom-elements/dist/web-components-hi.js')
+  document.head.appendChild(scriptEl);
+}
+if (!(window.customElements) {
+  let scriptEl = document.createElement('script');
+  scriptEl.setAttribute('src', '//unpkg.com/custom-elements/dist/ce-polyfill.js')
+  document.head.appendChild(scriptEl);
+}
+</script>
+
 <template for="CECore">
   <style id="ce-core-style">
   ${coreCss}
   </style>
-  <script>
-  (function() {
-    var thisScript = document.currentScript;
-    let template = thisScript.ownerDocument.querySelector('template[for=CECore]');
-    let style = template.content.querySelector('style').cloneNode(true);
-    !document.getElementById(style.id) && document.head.appendChild(style);
-  })();
-  </script>
 </template>
+
+<template for="XXXX"><style id="x-xxx-xx"></style></template><script> ... </script>
+<template for="XXXX"><style id="x-xxx-xx"></style></template><script> ... </script>
 */
+
+dist/
+  web-components-hi.js
+  ce-polyfill.js
+  core-elements.html (minified + polyfill)
+  core/
+    app-bar.html (minified + polyfill)
+    card.html (minified + polyfill)
+    ...
