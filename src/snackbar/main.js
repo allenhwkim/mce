05f1cb1c0cb8dbfa77d6c1612519dbe4a80af261
Allen Kim <allen.kim@rci.rogers.ca>
2018-01-15 21:08:58 -0500

    updated snackbar with configurable bottom distance

--- src/snackbar/main.js
+++ src/snackbar/main.js
@@ -16,6 +16,10 @@ import {observeAttrChange} from '../mce-util.js';
    * <p data-height="300" data-theme-id="32189" data-slug-hash="??????" data-default-tab="result" data-user="allenhwkim" data-embed-version="2" data-pen-title="mce template" class="codepen">See the Pen <a href="https://codepen.io/allenhwkim/pen/PEJKKo/">mce template</a> by Allen kim (<a href="https://codepen.io/allenhwkim">@allenhwkim</a>) on <a href="https://codepen.io">CodePen</a>.</p>
 <script async src="https://production-assets.codepen.io/assets/embed/ei.js"></script>
    *
+   * ### `mce-snackbar` attributes
+   *  |name|value|description|
+   *  |---|---|--|
+   *  |bottom|number|distance from bottom in pixel. e.g. 30|
    * 
    * ### `mce-snackbar` classes 
    *  |name|description|
@@ -26,7 +30,8 @@ import {observeAttrChange} from '../mce-util.js';
 
   class Snackbar extends HTMLElement {
     connectedCallback() {
-      this.open()
+      this.bottom = this.getAttribute('bottom') || '56';
+      this.open();
     }
 
     set message(msg) {
@@ -39,6 +44,7 @@ import {observeAttrChange} from '../mce-util.js';
 
     open(msg) {
       msg && (this.message = msg);
+      this.style.bottom  = this.bottom + 'px';
       this.classList.add('mce-visible');
       setTimeout(_ => {
         this.classList.remove('mce-visible');
