7296e9364dad7e17bc2c5b90d56fbc5391c791e0
Allen Kim <allenhwkim@gmail.com>
2018-09-28 13:41:48 -0400

    fixed IE11 bug el.constructor.name

--- dist/mce.js
+++ dist/mce.js
@@ -4444,6 +4444,22 @@ customElements.define('mce-list', List);
 /**
  * Allen's Custom Element Polyfill for better performance
  */
+
+/**
+ * Hack in support for Function.name for browsers that don't support it.
+ * IE, I'm looking at you.
+**/
+if (Function.prototype.name === undefined && Object.defineProperty !== undefined) {
+  Object.defineProperty(Function.prototype, 'name', {
+    get: function get() {
+      var funcNameRegex = /function\s([^(]{1,})\(/;
+      var results = funcNameRegex.exec(this.toString());
+      return results && results.length > 1 ? results[1].trim() : "";
+    },
+    set: function set(value) {}
+  });
+}
+
 (function () {
   var __customElements = {};
   var debug = !!window.location.search.match(/\?debug=true/);
--- src/mce-polyfill.js
+++ src/mce-polyfill.js
@@ -1,6 +1,22 @@
 /**
  * Allen's Custom Element Polyfill for better performance
  */
+
+/**
+ * Hack in support for Function.name for browsers that don't support it.
+ * IE, I'm looking at you.
+**/
+if (Function.prototype.name === undefined && Object.defineProperty !== undefined) {
+    Object.defineProperty(Function.prototype, 'name', {
+        get: function() {
+            var funcNameRegex = /function\s([^(]{1,})\(/;
+            var results = (funcNameRegex).exec((this).toString());
+            return (results && results.length > 1) ? results[1].trim() : "";
+        },
+        set: function(value) {}
+    });
+}
+
 (function(){
   let __customElements = {};
   let debug = !!window.location.search.match(/\?debug=true/);
